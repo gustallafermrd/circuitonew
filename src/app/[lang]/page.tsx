faec4d51ea4c14ee177fdab46fe593bc9322f2cb
gustallafermrd <36135271+gustallafermrd@users.noreply.github.com>
2026-01-21 07:27:34 -0300

    added gift card section

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -5,6 +5,7 @@ import Fundadores from "@/app/components/home/fundadores";
 import Mapa from "@/app/components/home/mapa";
 import Comentarios from "@/app/components/comentarios";
 import { getDictionary } from "@/lib/get-dictionary";
+import GiftCard from "@/app/components/home/gift-card";
 
 export default async function LangHome({
   params,
@@ -18,6 +19,7 @@ export default async function LangHome({
     <main className="flex-1 flex flex-col items-center w-full">
       <Hero dictionary={dictionary.hero} />
       <Intro dictionary={dictionary.intro} />
+      <GiftCard dictionary={dictionary.giftCard} />
       <Fundadores dictionary={dictionary.fundadores} lang={lang} />
       <Mapa />
       <Comentarios />
